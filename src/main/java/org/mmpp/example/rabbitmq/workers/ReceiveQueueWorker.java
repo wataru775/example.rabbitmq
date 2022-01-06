a121a4de12aa262eb8165126de4e901cab91e93b
wataru <wataru775@gmail.com>
2022-01-07 08:28:21 +0900

    callback処理をメソッドに

--- src/main/java/org/mmpp/example/rabbitmq/workers/ReceiveQueueWorker.java
+++ src/main/java/org/mmpp/example/rabbitmq/workers/ReceiveQueueWorker.java
@@ -1,14 +1,12 @@
 package org.mmpp.example.rabbitmq.workers;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeoutException;
 
 @Component
@@ -31,22 +29,25 @@ public class ReceiveQueueWorker {
         final Connection connection = factory.newConnection();
         final Channel channel = connection.createChannel();
 
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            logger.info( " Received '" + message + "'");
-            for (char ch: message.toCharArray()) {
-                if (ch == '.') {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
+        DeliverCallback deliverCallback = this::work;
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 
 //        channel.close();
 //        connection.close();
+    }
+
+    public void work(String consumerTag, Delivery delivery) throws UnsupportedEncodingException {
+        String message = new String(delivery.getBody(), "UTF-8");
+        logger.info( " Received '" + message + "'");
+        for (char ch: message.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
